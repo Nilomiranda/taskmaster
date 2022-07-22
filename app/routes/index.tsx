4a2709f374323efbd5b92df5db0477da85190177
Danilo <nilomiranda3@gmail.com>
2022-07-21 22:04:00 -0300

    chore: initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Taskmaster</h1>
+      <p>This will be entry point of a great application</p>
+    </div>
+  );
+}
