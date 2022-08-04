9a785ac63f1ec5d554bee56776dc6b3cd2dc9d25
Danilo <nilomiranda3@gmail.com>
2022-08-03 21:53:46 -0300

    chore: added links to account page and login on sign-out

--- app/components/layout/HorizontalNavBar.tsx
+++ app/components/layout/HorizontalNavBar.tsx
@@ -1,4 +1,5 @@
 import { Avatar, HStack, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function HorizontalNavBar() {
     return (
@@ -7,8 +8,12 @@ export default function HorizontalNavBar() {
                 <MenuButton cursor="pointer" as={Avatar} name="Danilo" src="https://picsum.photos/200s" />
 
                 <MenuList>
-                    <MenuItem>My account</MenuItem>
-                    <MenuItem>Sign out</MenuItem>
+                    <MenuItem>
+                        <Link to="/account">My account</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link to="/login">Sign out</Link>
+                    </MenuItem>
                 </MenuList>
             </Menu>
         </HStack>
