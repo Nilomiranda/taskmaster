d05e0e108bcc7cdb6aa15801227079699f70c00c
Danilo <nilomiranda3@gmail.com>
2022-08-03 22:15:16 -0300

    chore: grouping links

--- app/components/layout/VerticalNavBar.tsx
+++ app/components/layout/VerticalNavBar.tsx
@@ -1,6 +1,16 @@
-import { Box, HStack, Text, VStack } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { HStack, StackDivider, Text, VStack } from "@chakra-ui/react";
 import VerticalNavBarLinkItem from "./VerticalNavBarLinkItem";
+
+const linksGroups = [
+    [
+        { link: '/home/tasks', label: 'My tasks' },
+        { link: '/home/completed-tasks', label: 'Completed tasks' },
+    ],
+
+    [
+        { link: '/home/personas', label: 'Personas' },
+    ],
+]
 
 export default function VerticalNavBar() {
     return (
@@ -12,8 +22,22 @@ export default function VerticalNavBar() {
                 </VStack>
                 <br />
 
-                <VerticalNavBarLinkItem link="/home/tasks" label="My tasks" />
-                <VerticalNavBarLinkItem link="/home/completed-tasks" label="Completed tasks" />
+                <VStack alignItems="stretch" spacing="1.5rem" divider={<StackDivider borderColor='blue.900' width="100%" />}>
+                    {
+                        linksGroups.map((groupLink, index) => (
+                            <VStack alignItems="stretch" width="100%">
+                                {groupLink.map((link, linkIndex) => (
+                                    <VStack>
+                                        <VerticalNavBarLinkItem link={link.link} label={link.label} />
+                                    </VStack>
+                                ))}
+                            </VStack>
+                        ))
+                    }
+                </VStack>
+
+                {/* <VerticalNavBarLinkItem link="/home/tasks" label="My tasks" />
+                <VerticalNavBarLinkItem link="/home/completed-tasks" label="Completed tasks" /> */}
             </VStack>
         </HStack>
     )
