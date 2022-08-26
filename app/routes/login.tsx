8c89b2e21a9b74e4ab380726b359ad06caa133bb
Danilo <nilomiranda3@gmail.com>
2022-08-25 22:54:15 -0300

    feature: add input validation

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,9 +1,22 @@
-import { Button, Heading, Text, VStack } from "@chakra-ui/react";
+import { Button, Heading, Text, VStack, Input as CInput } from "@chakra-ui/react";
 import { Link } from "@remix-run/react";
 import Input from "~/components/form/Input";
 import Centered from "~/components/layout/Centered";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const validationSchema = yup.object().shape({
+    email: yup.string().email('Invalid email').required('Email is required'),
+    password: yup.string().required('Password is required')
+})
 
 export default function Login() {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+    const onSubmit = (data: any) => console.log(data);
+
     return (
         <Centered>
             <VStack mb="5rem">
@@ -11,11 +24,11 @@ export default function Login() {
                 <Heading as="h2" size="lg" color="gray.500">Get stuff done.</Heading>
             </VStack>
 
-            <VStack as="form" align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px">
+            <VStack as="form" align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px" onSubmit={handleSubmit(onSubmit)}>
                 <Text align="center" fontSize="xl">Login</Text>
 
-                <Input type="email" label="Email" placeholder="name@example.com" />
-                <Input type="email" label="Password" placeholder="******" />
+                <Input {...register('email')} id="email" type="email" label="Email" placeholder="name@example.com" errorMessage={errors.email?.message} />
+                <Input type="password" label="Password" placeholder="******" errorMessage={errors.password?.message} {...register('password')} />
 
                 <Button type="submit">Login</Button>
 
