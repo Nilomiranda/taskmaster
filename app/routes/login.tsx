a5497667dca342bed1df8639da08b2624e0ce7e0
Danilo Miranda <nilomiranda3@gmail.com>
2023-04-12 10:31:02 -0300

    chore (WIP): validation

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,16 +1,41 @@
 import { Button, Heading, Text, VStack, Input as CInput } from "@chakra-ui/react";
-import { Link } from "@remix-run/react";
+import { Link, Form } from "@remix-run/react";
 import Input from "~/components/form/Input";
 import Centered from "~/components/layout/Centered";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+import type {LoaderArgs} from "@remix-run/node";
+import {commitSession, getSession} from "~/sessions";
+import {json, redirect} from "@remix-run/node";
 
 const validationSchema = yup.object().shape({
     email: yup.string().email('Invalid email').required('Email is required'),
     password: yup.string().required('Password is required')
 })
 
+export async function loader({ request }: LoaderArgs) {
+  console.log('login loader')
+  const session = await getSession(request.headers.get('Cookie'));
+
+  console.log({ session })
+
+  if (session.has('userId')) {
+    return redirect('/home')
+  }
+
+  const data = { error: session.get('error') }
+
+  console.log({ data })
+
+  return json(data, {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    }
+  })
+
+  return null
+}
 export default function Login() {
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validationSchema)
@@ -24,7 +49,7 @@ export default function Login() {
                 <Heading as="h2" size="lg" color="gray.500">Get stuff done.</Heading>
             </VStack>
 
-            <VStack as="form" align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px" onSubmit={handleSubmit(onSubmit)}>
+            <VStack as={Form} align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px" onSubmit={handleSubmit(onSubmit)}>
                 <Text align="center" fontSize="xl">Login</Text>
 
                 <Input {...register('email')} id="email" type="email" label="Email" placeholder="name@example.com" errorMessage={errors.email?.message} />
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,9 +1,36 @@
 import { Button, Heading, Text, VStack } from "@chakra-ui/react";
-import { Link } from "@remix-run/react";
+import { Link, Form } from "@remix-run/react";
 import Input from "~/components/form/Input";
 import Centered from "~/components/layout/Centered";
+import type {ActionArgs} from "@remix-run/node";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
+const validationSchema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup.string().required('Password is required'),
+  passwordConfirmation: yup.string().test('password-match', 'Passwords must match', function (value) {
+    return this.parent.password === value;
+  })
+})
+// export async function action({ request }: ActionArgs) {
+//   const form = await request.formData();
+//   const [name, email, password, passwordConfirmation] = ['name', 'email', 'password', 'passwordConfirmation'].map(formFieldName => form.get(formFieldName));
+//
+// console.log({ name, email, password, passwordConfirmation})
+//
+//   return null;
+// }
 export default function SignUp() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = (data: any) => console.log(data);
+
+
     return (
         <Centered>
             <VStack mb="5rem">
@@ -11,13 +38,13 @@ export default function SignUp() {
                 <Heading as="h2" size="lg" color="gray.500">Get stuff done.</Heading>
             </VStack>
 
-            <VStack as="form" align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px">
+            <VStack action="/auth/signup" method="post" as={Form} align="stretch" spacing="2rem" padding="1rem" width="100%" maxWidth="400px" onSubmit={() => handleSubmit(onSubmit)}>
                 <Text align="center" fontSize="xl">Sign up</Text>
 
-                <Input placeholder="Your name" type="text" label="Name" />
-                <Input placeholder="name@example.com" type="email" label="Email" errorMessage="Invalid email" />
-                <Input placeholder="A safe password" type="password" label="Password" />
-                <Input placeholder="Confirm your chosen password" type="password" label="Confirm password" />
+                <Input {...register('name')} errorMessage={errors?.name?.message} placeholder="Your name" type="text" label="Name" name="name" />
+                <Input {...register('email')} errorMessage={errors?.email?.message} placeholder="name@example.com" type="email" label="Email" name="email" />
+                <Input {...register('password')} errorMessage={errors?.password?.message} placeholder="A safe password" type="password" label="Password" name="password" />
+                <Input {...register('passwordConfirmation')} errorMessage={errors?.passwordConfirmation?.message} placeholder="Confirm your chosen password" type="password" label="Confirm password" name="passwordConfirmation" />
 
                 <Button type="submit">Create account</Button>
 
