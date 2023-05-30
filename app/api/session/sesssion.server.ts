dd34f7263f75043ab2f5a60cd43aac675b7b5aff
Danilo Miranda <nilomiranda3@gmail.com>
2023-05-30 20:29:23 -0300

    feature: login flow

--- app/api/session/sesssion.server.ts
+++ app/api/session/sesssion.server.ts
@@ -1,5 +1,12 @@
 import {commitSession, getSession} from "~/sessions";
 import {json, redirect} from "@remix-run/node";
+import {prisma} from "~/db/prismaClient";
+import * as brcrypt from 'bcryptjs'
+
+export interface CreateSessionPayload {
+    email: string;
+    password: string;
+}
 
 export async function checkSessionAndRedirect(request: Request) {
     const session = await getSession(request.headers.get('Cookie'));
@@ -17,4 +24,37 @@ export async function checkSessionAndRedirect(request: Request) {
     })
 
     return null
+}
+
+export async function createSession(request: Request, payload: CreateSessionPayload) {
+    const session = await getSession(request.headers.get('Cookie'));
+
+    const { email, password } = payload;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+
+    if (!user) {
+        throw json('Wrong credentials', { status: 401 })
+    }
+
+    const { password: hashedPassword } = user;
+
+    const isPasswordValid = await brcrypt.compare(password, hashedPassword);
+
+    if (!isPasswordValid) {
+        throw json('Wrong credentials', { status: 401 })
+    }
+
+    const { id } = user;
+    session.set('userId', id)
+
+    return redirect('/home', {
+        headers: {
+            'Set-Cookie': await commitSession(session)
+        }
+    })
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,12 +1,14 @@
-import { Button, Heading, Text, VStack } from "@chakra-ui/react";
-import { Link, Form } from "@remix-run/react";
+import {Button, Heading, Text, useToast, VStack} from "@chakra-ui/react";
+import {Link, Form, useSearchParams, useSubmit} from "@remix-run/react";
 import Input from "~/components/form/Input";
 import Centered from "~/components/layout/Centered";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import type {LoaderArgs} from "@remix-run/node";
-import {checkSessionAndRedirect} from "~/api/session/sesssion.server";
+import {checkSessionAndRedirect, createSession} from "~/api/session/sesssion.server";
+import {ActionArgs, redirect} from "@remix-run/node";
+import {useEffect} from "react";
 
 const validationSchema = yup.object().shape({
     email: yup.string().email('Invalid email').required('Email is required'),
@@ -16,11 +18,50 @@ const validationSchema = yup.object().shape({
 export async function loader({ request }: LoaderArgs) {
     return checkSessionAndRedirect(request);
 }
+
+export async function action({ request }: ActionArgs) {
+    const form = await request.formData();
+    const [
+        email,
+        password,
+    ] = ['email', 'password'].map(formFieldName => form.get(formFieldName));
+
+    try {
+        await createSession(request, { email: String(email), password: String(password) })
+    } catch (err: any) {
+        const errorMessage = await err?.json();
+
+        return redirect(`/login?error=${errorMessage}`)
+    }
+}
 export default function Login() {
+    const toast = useToast();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const error = searchParams.get('error')
+
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validationSchema)
     });
-    const onSubmit = (data: any) => console.log(data);
+
+    const formSubmit = useSubmit();
+    const onSubmit = (data: any) => {
+        formSubmit(data as unknown as Record<string, string>, { action: '/login', method: 'post' })
+    };
+
+    useEffect(() => {
+        if (error) {
+            toast({
+                title: 'Error signing in.',
+                description: error,
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            })
+
+            searchParams.delete('error')
+            setSearchParams(searchParams)
+        }
+    }, [toast, error, searchParams, setSearchParams])
 
     return (
         <Centered>
