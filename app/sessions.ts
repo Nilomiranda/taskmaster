37946982d7f0bc37892259b1ca603410620e1c8a
Danilo Miranda <nilomiranda3@gmail.com>
2023-06-26 11:39:16 -0300

    chore: set cookie duration to 1 week

--- app/sessions.ts
+++ app/sessions.ts
@@ -1,10 +1,12 @@
 import {createCookieSessionStorage} from "@remix-run/node";
 
+const COOKIE_ONE_WEEK_MAX_AGE = 60 * 60 * 24 * 7;
+
 const { getSession, commitSession, destroySession } = createCookieSessionStorage({
   cookie: {
     name: "taskmaster__session",
     httpOnly: true,
-    maxAge: 60,
+    maxAge: COOKIE_ONE_WEEK_MAX_AGE,
     path: '/',
     sameSite: 'lax',
     secure: true,
